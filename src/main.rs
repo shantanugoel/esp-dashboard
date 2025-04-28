bec343406ed5fc0564688b16a6e6fe5b97de6d5b
Shantanu Goel <shantanu.goel@gmail.com>
2025-04-28 22:16:31 +0530

    Wifi

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@ use waveshare_esp32_s3_lcd_7_bsp_rs::{http, wifi, slint_platform};
 
 slint::include_modules!();
 
+#[toml_cfg::toml_config]
+struct AppConfig {
+    #[default("")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_password: &'static str,
+}
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -30,6 +38,12 @@ fn main() {
 
     let window = MainWindow::new().unwrap();
     let window_handle = window.as_weak();
+
+    let event_loop = esp_idf_svc::eventloop::EspSystemEventLoop::take().expect("Failed to create event loop");
+    let nvs_partition = esp_idf_svc::nvs::EspDefaultNvsPartition::take().expect("Failed to create NVS partition");
+    let mut wifi_driver = wifi::init(peripherals.modem, event_loop, Some(nvs_partition)).expect("Failed to create wifi driver");
+    wifi::connect(&mut wifi_driver, APP_CONFIG.wifi_ssid, APP_CONFIG.wifi_password).expect("Failed to connect to wifi");
+    wifi::wait_for_connection(&mut wifi_driver).expect("Failed to wait for connection");
 
     window.run().unwrap();
 }
