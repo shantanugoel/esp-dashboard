79eacdccc7524254e57dd7989143b3192c559a71
Shantanu Goel <shantanu.goel@gmail.com>
2025-04-28 22:34:28 +0530

    http

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{thread::sleep, time::Duration};
 use esp_idf_svc::hal::prelude::*;
+use serde_json::Value;
+use slint::SharedString;
 use waveshare_esp32_s3_lcd_7_bsp_rs::{http, wifi, slint_platform};
 
 slint::include_modules!();
@@ -33,9 +34,6 @@ fn main() {
 
     slint_platform::init(touch_i2c);
 
-    let mut timer = esp_idf_svc::hal::timer::TimerDriver::new(peripherals.timer00
-    , &Default::default()).expect("Failed to create timer");
-
     let window = MainWindow::new().unwrap();
     let window_handle = window.as_weak();
 
@@ -44,6 +42,19 @@ fn main() {
     let mut wifi_driver = wifi::init(peripherals.modem, event_loop, Some(nvs_partition)).expect("Failed to create wifi driver");
     wifi::connect(&mut wifi_driver, APP_CONFIG.wifi_ssid, APP_CONFIG.wifi_password).expect("Failed to connect to wifi");
     wifi::wait_for_connection(&mut wifi_driver).expect("Failed to wait for connection");
+
+    let mut client = http::HttpClient::new().expect("Failed to create http client");
+    let headers = [("Content-Type", "application/json")];
+    let url = "https://api.chucknorris.io/jokes/random";
+
+    window.on_update_fact(move || {
+        let body = client.get(url, &headers).expect("Failed to get body");
+        log::info!("{}", body);
+        let v: Value = serde_json::from_str(&body).expect("Failed to parse json");
+        let fact = v["value"].as_str().unwrap_or("No fact found");
+        let window = window_handle.upgrade().unwrap();
+        window.set_fact(SharedString::from(fact));
+    });
 
     window.run().unwrap();
 }
